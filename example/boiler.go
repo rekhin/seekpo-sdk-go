3426d15bee2c009f1406f6ca7e309d46fbe5cabb
Sergey Terekhin <goonya@ya.ru>
2021-11-01 18:43:49 +0300

    add example IM tree

--- example/boiler.go
+++ example/boiler.go
@@ -1,6 +1,6 @@
 // .
-// └── organization/
-//     └── boiler/
+// └── Organization/
+//     └── HotWaterSupply/
 //         ├── input/
 //         │   ├── temperature
 //         │   └── pressure
@@ -8,16 +8,74 @@
 //             ├── temperature
 //             └── pressure
 
+// # Seekpo=Organization
+// ## Hotel=Building
+// ### Boiler=HotWaterSupply
+// #### Direct=Pipe
+// ##### Inpup
+// ###### Temperature
+// ###### Pressure
+// ###### VolumeFlowRate
+// ###### MassFlowRate
+// ##### Output
+// ###### Enthalpy
+// ###### Density
+// ###### MassFlow
+// #### Reverse=Pipe
+// ##### Inpup
+// ###### Temperature
+// ###### Pressure
+// ###### VolumeFlowRate
+// ###### MassFlowRate
+// ##### Output
+// ###### Enthalpy
+// ###### Density
+// ###### MassFlow
+
+// .
+// └── Seekpo=Organization
+//     └── Hotel=Building
+//         └── Boiler=HotWaterSupply
+//             ├── Direct=Pipe
+//             │   ├── Temperature=floa64
+//             │   ├── Pressure=floa64
+//             │   ├── VolumeFlowRate=floa64
+//             │   ├── MassFlowRate=floa64
+//             │   ├── Enthalpy=floa64
+//             │   ├── Density=floa64
+//             │   └── MassFlow=floa64
+//             └── Reverse=Pipe
+//                 ├── Temperature=floa64
+//                 ├── Pressure=floa64
+//                 ├── VolumeFlowRate=floa64
+//                 ├── MassFlowRate=floa64
+//                 ├── Enthalpy=floa64
+//                 ├── Density=floa64
+//                 └── MassFlow=floa64
+
 package main
 
-type Boiler struct {
-	Input  Pipe
-	Output Pipe
+type HotWaterSupply struct {
+	Direct  Pipe
+	Reverse Pipe
 }
 
 type Pipe struct {
-	Temperature float32
-	Pressure    float32
+	Inpup  Input
+	Output Output
+}
+
+type Input struct {
+	Temperature    float64
+	Pressure       float64
+	VolumeFlowRate float64
+	MassFlowRate   float64
+}
+
+type Output struct {
+	Enthalpy float64
+	Density  float64
+	MassFlow float64
 }
 
 // Hotel=building/Boiler=boiler/Input=pipe/Temperature=float32
